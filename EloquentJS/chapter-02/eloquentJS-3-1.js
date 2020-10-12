1b87679b9c850739b74d3763049632ee192536bb
mariel-delagarza <mariel.code@gmail.com>
2020-10-12 13:32:22 -0400

    Add chapter 3 exercises for eloquentJS

---
+++ EloquentJS/chapter-02/eloquentJS-3-1.js
@@ -0,0 +1,17 @@
+/* 
+
+Minimum: 
+  Chapter 2 introduced the standard function Math.min, which returns its smallest argument. 
+  We can build something like that now. 
+  Write a function "min" that takes two arguments and returns their minimum.
+
+*/
+
+// Solution
+
+function min(a, b) {
+  if (a < b) {
+    return a
+  } else 
+  return b
+}
